857019e2557e430dc30206b24448cadf3feff29f
Ben Thomas <Benjamingathomas@gmail.com>
2024-11-23 16:01:47 -0500

    completed styling skills and added picture

--- bengathomas/src/components/benThomas.jsx
+++ bengathomas/src/components/benThomas.jsx
@@ -4,19 +4,22 @@ const BenT = () => {
     return (
         <section>
             <div className='flex flex-col items-center justify-center' >
-                <h1 className="mt-16 overflow-hidden text-[6vw] font-semibold uppercase leading-none">
+                <h1 className="mt-12 overflow-hidden text-[3vw] font-semibold uppercase leading-none">
                     Benjamin <br /> Thomas
                 </h1>
             {/* <img src='./pictures/atlutd_logo.png' alt='photo of Ben'></img> */}
             <div className="mt-8">
-                <a href="./pictures/Benjamin Thomas Resume.pdf" 
+                <a href="./assets/Benjamin Thomas Resume.pdf" 
                     target="_blank" 
                     rel="noopener noreferrer"
                     download
-                    className="flex items-center rounded-xl bg-green-500 p-2 px-3 font-sans font-medium text-black hover:bg-green-800"></a>
-                    <span>Resume.pdf</span>
-                    <LuImport className="ml-2" />
-                <a/>
+                    className="flex items-center rounded-xl bg-green-500 p-3 px-3 font-sans font-medium text-black hover:bg-green-800">
+                        <span>Resume.pdf</span>
+                        <LuImport className="ml-2" />
+                    </a>
+            </div>
+            <div className="w-50">
+                <img src="./assets/atlutd_logo.png" alt="Benjamin Thomas" className="mt-8 h-100 w-50 object-cover"/>
             </div>
             <p className='about'>My name is Benjamin Thomas. I am a software engineer living in Atlanta, GA. I have experience working with JavaScript, TypeScript, React, NextJS, MongoDB and MYSQL. I have a desire to learn new languages and growing my knowledge of web development and web design. Below you can see some of the projects I have worked on.</p>
             </div>
