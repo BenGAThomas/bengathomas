dd303b8fddedd4744fba79486f4b54dea18a12c4
Benjamin Thomas <Benjamingathomas@gmail.com>
2024-11-27 00:38:17 -0500

    need to finish adding projects

--- bengathomas/src/App.jsx
+++ bengathomas/src/App.jsx
@@ -5,15 +5,7 @@ import Projects from './components/projects';
 import Contact from './components/contact';
 import Marquee from './components/Marquee';
 
-// export const LINKS = [
-//   {id: "projects", name: "projects"},
-//   {id: "about", name: "about"},
-//   {id: "experience", name: "experience"},
-//   {id: "contact", name: "contact"}
-// ]
 
-// export const SKILLS =
-//   "React, Node, Tailwind, Javascript, HTML/CSS, MYSQL, Angular, Postgres, MongoDB, AWS"
 
 function App() {
 
