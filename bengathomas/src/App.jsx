1fc527032f598309ef7523b215352f33d75777b2
Ben Thomas <Benjamingathomas@gmail.com>
2024-11-29 15:47:21 -0500

    finished project and about page

--- bengathomas/src/App.jsx
+++ bengathomas/src/App.jsx
@@ -4,6 +4,7 @@ import BenT from './components/benThomas';
 import Projects from './components/projects';
 import Contact from './components/contact';
 import Marquee from './components/Marquee';
+import About from './components/About';
 
 
 
@@ -15,6 +16,8 @@ function App() {
       <NavBar />
 
       <BenT />
+
+      <About />
 
       <Marquee />
       
--- bengathomas/src/components/contact.jsx
+++ bengathomas/src/components/contact.jsx
@@ -3,12 +3,16 @@ import { FaLinkedin } from "react-icons/fa"
 
 const Contact = () => {
     return (
-    <div className='contactInfo'>
-    <h1>Contact Me</h1>
+    <section id='contact'>
+      <div className="mx-auto max-w-6xl">
+        <h1 className="my-10 text-center text-2xl lg:text-4xl">Contact Me</h1>
+        <p className="my-4 text-center text-xl">I am continuously looking for the opportunity to freelance or contract work on building your website or creating projects. Feel free to reach out to me regarding work so we can build your website.</p>
+      </div>
+    
     <h3>Benjamingathomas@gmail.com</h3>
     <a href='https://www.linkedin.com/in/benjamingathomas/'><FaLinkedin></FaLinkedin></a> 
     <a href="http://www.github.com/BenGAThomas"><FaGithub></FaGithub></a>
-  </div>
+  </section>
   )
 }
 
