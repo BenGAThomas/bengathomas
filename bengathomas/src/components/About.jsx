b81040fb02679b4f37f19dd4784dd59caa05ee5c
Ben Thomas <Benjamingathomas@gmail.com>
2025-01-22 22:14:24 -0500

    changed the styling to look better

--- bengathomas/src/components/About.jsx
+++ bengathomas/src/components/About.jsx
@@ -3,7 +3,7 @@ const About = () => {
     <section id="about">
         <h2 className="my-7 text-center text-3xl lg:text-4xl">About Me</h2>
         <div className="flex items-center justify-center">
-            <p className="m-1 max-w-6xl text-3xl lg:text-2xl">I am a Software Engineer located in Atlanta, GA. I specialize in creating responsive website and providing exceptional user experiences. I have a strong comprehension of front-end technologies. My skill set is in frameworks such as Javascript, React and Angular. I also have proficiency in SQL databases such as MongoDB, and Postgres. I am enthusiastic about learning new frameworks, libraries, and technology to help keep me up to date on the latest trends in building scalable, and great user friendly websites.</p>
+            <p className="m-1 max-w-6xl text-3xl lg:text-2xl">I am a Software Engineer located in Atlanta, GA. I specialize in creating responsive website and providing exceptional user experiences. I have a strong comprehension of front-end technologies. My skill set is in frameworks such as Javascript, React, and NextJS. I also have proficiency in SQL databases such as MongoDB, and Postgres. I am enthusiastic about learning new frameworks, libraries, and technology to help keep me up to date on the latest trends in building scalable, and great user friendly websites.</p>
         </div>
     </section>
   )
--- bengathomas/src/components/benThomas.jsx
+++ bengathomas/src/components/benThomas.jsx
@@ -14,14 +14,14 @@ const BenT = () => {
                     rel="noopener noreferrer"
                     download
                     className="flex items-center rounded-xl bg-green-500 p-3 px-3 font-sans font-medium text-black hover:bg-green-800">
-                        <span>Resume.pdf</span>
+                        <span>Benjamin Thomas Resume.pdf</span>
                         <LuImport className="ml-2" />
                     </a>
             </div>
             <div className="w-50">
-                <img src="./assets/atlutd_logo.png" alt="Benjamin Thomas" className="mt-8 h-100 w-50 object-cover"/>
+                {/*<img src="./assets/atlutd_logo.png" alt="Benjamin Thomas" className="mt-8 h-100 w-50 object-cover"/>*/ }
             </div>
-            <p className='about'>My name is Benjamin Thomas. I am a software engineer living in Atlanta, GA. I have experience working with JavaScript, TypeScript, React, NextJS, MongoDB and MYSQL. I have a desire to learn new languages and growing my knowledge of web development and web design. Below you can see some of the projects I have worked on.</p>
+            {/*<p className='about'>My name is Benjamin Thomas. I am a software engineer living in Atlanta, GA. I have experience working with JavaScript, TypeScript, React, NextJS, MongoDB and MYSQL. I have a desire to learn new languages and growing my knowledge of web development and web design. Below you can see some of the projects I have worked on.</p>*/}
             </div>
         </section>
 )
