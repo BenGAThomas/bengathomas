f366088cbce9ef1a150aa4d8f389a35fe6757bd0
Ben Thomas <Benjamingathomas@gmail.com>
2025-02-01 21:13:26 -0500

    changing up the styling

--- bengathomas/src/components/About.jsx
+++ bengathomas/src/components/About.jsx
@@ -1,9 +1,9 @@
 const About = () => {
   return (
     <section id="about">
-        <h2 className="my-7 text-center text-3xl lg:text-4xl">About Me</h2>
+        <h2 className="my-7 text-center text-3xl lg:text-4xl">Who I am</h2>
         <div className="flex items-center justify-center">
-            <p className="m-1 max-w-6xl text-3xl lg:text-2xl">I am a Software Engineer located in Atlanta, GA. I specialize in creating responsive website and providing exceptional user experiences. I have a strong comprehension of front-end technologies. My skill set is in frameworks such as Javascript, React, and NextJS. I also have proficiency in SQL databases such as MongoDB, and Postgres. I am enthusiastic about learning new frameworks, libraries, and technology to help keep me up to date on the latest trends in building scalable, and great user friendly websites.</p>
+            <p className="m-1 max-w-6xl text-3xl lg:text-2xl">I am a Software Engineer located in Atlanta, GA. I specialize in creating responsive website and providing exceptional user experiences. I have a strong comprehension of front-end technologies. My skill set is in Javascript, React, NextJS and Python. I also have proficiency in SQL databases such as MongoDB, and Postgres. I am enthusiastic about learning new frameworks, libraries, and technology to help keep me up to date on the latest trends in building scalable, and great user friendly websites. In my free time I enjoy playing board/video games, D&D, soccer, solving jigsaw puzzles, and reading books. </p>
         </div>
     </section>
   )
